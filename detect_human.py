fa5857da11549caee09072b7a91c3359196db392
Robert Pham <robqpham@gmail.com>
2015-10-14 22:45:00 -0700

    save trained svm and testing classifier performance

--- detect_human.py
+++ detect_human.py
@@ -2,9 +2,13 @@ import cv2
 import numpy as np
 from sklearn import svm
 import hog
+import pickle
+import sys
 
 TRAIN_LIST_PATH = '../datasets/INRIAPerson/train_64x128_H96/'
+TEST_LIST_PATH = '../datasets/INRIAPerson/test_64x128_H96/'
 IMAGE_PATH = '../datasets/INRIAPerson/96X160H96/Train/'
+TEST_IMAGE_PATH = '../datasets/INRIAPerson/70X134H96/Test/'
 POS_FILENAME = 'pos.lst'
 NEG_FILENAME = 'neg.lst'
 
@@ -29,7 +33,7 @@ class HumanDetector:
     def build_features(self, pos_list, neg_list, show=False):
         for pos in pos_list:
             # Detect features with HoG
-            print 'pos', pos
+            # print 'pos', pos
             img = cv2.imread(pos, cv2.IMREAD_GRAYSCALE)
             img = np.float32(img)
             features = self.hog.descript(img)
@@ -43,7 +47,7 @@ class HumanDetector:
 
         for neg in neg_list:
             # Detect features with HoG
-            print 'neg', neg
+            # print 'neg', neg
             img = cv2.imread(neg, cv2.IMREAD_GRAYSCALE)
             img = np.float32(img)
             features = self.hog.descript(img)
@@ -72,43 +76,114 @@ class HumanDetector:
 
 
 # MAIN
+loading = False
+
+if(str(sys.argv[1]) == '-l'):
+    loading = True
+
+# TRAINING
 human_detector = HumanDetector()
 
-pos_list = []
-neg_list = []
-# Getting the list of positive samples
-with open(TRAIN_LIST_PATH + POS_FILENAME, 'r') as f:
-    while True:
-        line = f.readline()
-        # Zero length indicates EOF
-        if len(line) == 0:
-            break
+if loading:
+    with open('human_detector.pkl', 'rb') as input:
+        human_detector.clf = pickle.load(input)
+else:
+    pos_list = []
+    neg_list = []
+    # Getting the list of positive samples
+    with open(TRAIN_LIST_PATH + POS_FILENAME, 'r') as f:
+        while True:
+            line = f.readline()
+            # Zero length indicates EOF
+            if len(line) == 0:
+                break
 
-        # get filename without return
-        filename = line.rpartition('/')[2].rstrip()
-        pos_list.append(IMAGE_PATH + '/pos/' + filename)
+            # get filename without return
+            filename = line.rpartition('/')[2].rstrip()
+            pos_list.append(IMAGE_PATH + '/pos/' + filename)
 
-# Getting the list of negative samples
-with open(TRAIN_LIST_PATH + NEG_FILENAME, 'r') as f:
-    while True:
-        line = f.readline()
-        # Zero length indicates EOF
-        if len(line) == 0:
-            break
+    # Getting the list of negative samples
+    with open(TRAIN_LIST_PATH + NEG_FILENAME, 'r') as f:
+        while True:
+            line = f.readline()
+            # Zero length indicates EOF
+            if len(line) == 0:
+                break
 
-        # get filename without return
-        filename = line.rpartition('/')[2].rstrip()
-        neg_list.append(IMAGE_PATH + '/neg/' + filename)
+            # get filename without return
+            filename = line.rpartition('/')[2].rstrip()
+            neg_list.append(IMAGE_PATH + '/neg/' + filename)
 
-# Training
-# test one image for now
-human_detector.build_features(pos_list[:100], neg_list[:100])
-human_detector.train()
+    # Training
+    # test one image for now
+    print 'pos list length', len(pos_list)
+    print 'neg list length', len(neg_list)
+    human_detector.build_features(pos_list[:10], neg_list[:10], True)
+    human_detector.train()
+    with open('human_detector.pkl', 'wb') as output:
+        pickle.dump(human_detector.clf, output, pickle.HIGHEST_PROTOCOL)
 
-# Test an image
-test_img = '../datasets/INRIAPerson/96X160H96/Train/pos/crop_000010a.png'
-img = cv2.imread(test_img, cv2.IMREAD_GRAYSCALE)
-print 'is it a human?', human_detector.test(img)[0]
+# TESTING
+# Test the classifier
+pos_test_list = []
+with open(TEST_LIST_PATH + POS_FILENAME, 'r') as f:
+        while True:
+            line = f.readline()
+            # Zero length indicates EOF
+            if len(line) == 0:
+                break
+
+            # get filename without return
+            filename = line.rpartition('/')[2].rstrip()
+            pos_test_list.append(TEST_IMAGE_PATH + '/pos/' + filename)
+
+neg_test_list = []
+with open(TEST_LIST_PATH + NEG_FILENAME, 'r') as f:
+        while True:
+            line = f.readline()
+            # Zero length indicates EOF
+            if len(line) == 0:
+                break
+
+            # get filename without return
+            filename = line.rpartition('/')[2].rstrip()
+            neg_test_list.append(TEST_IMAGE_PATH + '/neg/' + filename)
+
+TP = 0.0
+FN = 0.0
+FP = 0.0
+TN = 0.0
+for pos in pos_test_list[:1]:
+    img = cv2.imread(pos, cv2.IMREAD_GRAYSCALE)
+    # print human_detector.test(img)[0]
+    # cv2.imshow('Image', np.uint8(img))
+    # cv2.waitKey(0)
+    if (human_detector.test(img)[0] == 1):
+        TP += 1.
+    else:
+        FN = FN + 1.
+
+for neg in neg_test_list[:1]:
+    img = cv2.imread(neg, cv2.IMREAD_GRAYSCALE)
+    if (human_detector.test(img)[0] == 1):
+        FP = FP + 1.
+    else:
+        TN = TN + 1.
+
+print 'neg', len(neg_test_list)
+print 'pos', len(pos_test_list)
+print '          pos   |  neg'
+print '------------------------'
+print 'pos |    {}     |  {}   '.format(TP, FN)
+print '------------------------'
+print 'neg |    {}     |  {}   '.format(FP, TN)
+print '------------------------'
+
+precision = TP/(TP + FP)
+accuracy = (TP + TN) / (TP + TN + FP + FN)
+print 'Precision:', precision
+print 'Accuracy:', accuracy
+
 
 # search image for human using sliding window approach
 # Then use non-maximal suppression to get one highest scoring detection
--- hog.py
+++ hog.py
@@ -13,6 +13,7 @@ class Hog:
     __ahist = 0
 
     def hog_show(self):
+        # only works for 96x1609
         cols, rows, bins = self.__ahist.shape
         print rows, cols
         img = np.zeros((rows*8, cols*8))
@@ -21,8 +22,6 @@ class Hog:
             for j in range(0, rows-1):
                 # get the angles
                 angle_weights = self.__ahist[i][j][:]
-                # normalize to 255
-                angle_weights = angle_weights / np.amax(self.__ahist) * 255
                 # print angle_weights
 
                 # draw the line
@@ -46,44 +45,41 @@ class Hog:
         return img
 
     def descript(self, img):
+        rows, cols = img.shape
+        numcells_h = 12
+        numcells_v = 20
+        cell_size_v = np.int8(rows/numcells_v)
+        cell_size_h = np.int8(cols/numcells_h)
+        img = cv2.resize(img, (cell_size_h * numcells_h,
+                               cell_size_v * numcells_v),
+                         interpolation=cv2.INTER_AREA)
+
         # get the gradients
         gx = cv2.filter2D(img, -1, np.mat('-1 0 1'))
         gy = cv2.filter2D(img, -1, np.mat('-1; 0.; 1.'))
-        # print 'gx', gx
         mag, ang = cv2.cartToPolar(gx, gy)  # angles 0 - 2pi
         ang[ang >= np.pi] = ang[ang >= np.pi] - np.pi
-        # plt.imshow(ang)
-        # plt.show()
-        # print 'ang shape:', ang.shape
 
         # quantize from 0...16
         bins = np.int32(self.__bin_n * ang / (np.pi))
         # print 'bin shape', bins.shape
 
         # separate image into cells
-        rows, cols = img.shape
-        numcells_h = cols/8
-        numcells_v = rows/8
         cells = np.array(np.hsplit(bins, numcells_h))
         mcells = np.array(np.hsplit(mag, numcells_h))
-        # print 'cells shape', cells.shape
-        bin_cells = np.empty((numcells_h, numcells_v, 8, 8))
-        mag_cells = np.empty((numcells_h, numcells_v, 8, 8))
-        # print 'self.__bin_cells', self.__bin_cells[:][:][:][:].shape
+        bin_cells = np.empty((numcells_h, numcells_v,
+                              cell_size_v, cell_size_h))
+        mag_cells = np.empty((numcells_h, numcells_v,
+                              cell_size_v, cell_size_h))
+
         for i in range(0, numcells_h):
-            # print 'what', np.array(np.vsplit(cells[:][i], 20)).shape
             bin_cells[i][:][:][:] = np.array(np.vsplit(cells[:][i],
                                              numcells_v))
             mag_cells[i][:][:][:] = np.array(np.vsplit(mcells[:][i],
                                              numcells_v))
-        # print 'bin cells', self.__bin_cells[10][19]
-        # print 'mag cells', self.__mag_cells[10][19]
 
-        # Calculate the block orientations
-        # i = 0
-        # print self.__bin_cells[i:2+i, i:2+i].ravel().shape
         hist = []
-        self.__ahist = np.empty((numcells_h, numcells_v, self.__bin_n))
+        self.__ahist = np.empty((numcells_h, numcells_v, self.__bin_n+1))
         for i in range(0, numcells_h-1):
             for j in range(0, numcells_v-1):
                 # print i
@@ -91,9 +87,12 @@ class Hog:
                 hists = np.bincount(
                     np.int32(bin_cells[i:2+i, j:2+j].ravel()),
                     weights=np.int32(mag_cells[i:2+i, j:2+j].ravel()),
-                    minlength=self.__bin_n)
+                    minlength=self.__bin_n+1)
                 # print 'hists', hists
-                self.__ahist[i][j][:] = np.array(hists)
+                if np.amax(hists) != 0.0:
+                    hists = hists / np.amax(hists) * 255
+                    print 'hists', hists
+                self.__ahist[i][j][:] = hists
                 # make the 1D vector.
                 hist.append(hists)
         # print np.array(hist).ravel().shape #20*12*16
