3f0d23e92f557e9a4d35709035bbe1189ad3344e
Robert Pham <robqpham@gmail.com>
2015-10-15 17:33:08 -0700

    Update display hog

--- hog.py
+++ hog.py
@@ -12,11 +12,17 @@ class Hog:
 
     __ahist = 0
 
+    # For display
+    __cell_size_v = 0
+    __cell_size_h = 0
+
     def hog_show(self):
         # only works for 96x160
         cols, rows, bins = self.__ahist.shape
+        cell_rows = self.__cell_size_v
+        cell_cols = self.__cell_size_h
         print rows, cols
-        img = np.zeros((rows*8, cols*8))
+        img = np.zeros((rows*cell_rows, cols*cell_cols))
         print img.shape
         for i in range(0, cols-1):
             for j in range(0, rows-1):
@@ -26,22 +32,24 @@ class Hog:
 
                 # draw the line
                 angle = 0
-                x = (1, 4)
-                y = (7, 4)
-                cell = np.zeros((8, 8))
+                x = (1, cell_rows/2)
+                y = (cell_cols-1, cell_rows/2)
+                cell = np.zeros((cell_rows, cell_cols))
                 for angle_weight in angle_weights:
-                    myline = np.zeros((8, 8))
+                    myline = np.zeros((cell_rows, cell_cols))
                     cv2.line(myline, x, y, (angle_weight, 0, 0), 1)
-                    M = cv2.getRotationMatrix2D((4, 4), angle, 1)
-                    myline = cv2.warpAffine(myline, M, (8, 8))
+                    cv2.imshow('line', myline)
+                    M = cv2.getRotationMatrix2D(
+                        (cell_cols/2, cell_rows/2), angle, 1)
+                    myline = cv2.warpAffine(myline, M, (cell_cols, cell_rows))
                     cell[cell < myline] = myline[cell < myline]
                     # img[j*8:(j+1)*8, i*8:(i+1)*8] =\
                     #    cv2.addWeighted(img[j*8:(j+1)*8, i*8:(i+1)*8], 0.5,
                     #                    cell, 0.5, 0.0)
-                    img[j*8:(j+1)*8, i*8:(i+1)*8] = cell
+                    img[j*cell_rows:(j+1)*cell_rows,
+                        i*cell_cols:(i+1)*cell_cols] = cell
 
                     angle += 180/self.__bin_n
-
         return img
 
     def descript(self, img):
@@ -51,6 +59,9 @@ class Hog:
         numcells_v = 20
         cell_size_v = np.int8(rows/numcells_v)
         cell_size_h = np.int8(cols/numcells_h)
+        self.__cell_size_v = cell_size_v
+        self.__cell_size_h = cell_size_h
+
         img = cv2.resize(img, (cell_size_h * numcells_h,
                                cell_size_v * numcells_v),
                          interpolation=cv2.INTER_AREA)
@@ -93,8 +104,8 @@ class Hog:
                     minlength=self.__bin_n+1)
 
                 # print 'hists', hists
-                # if np.amax(hists) != 0.0:
-                    # hists = hists / np.amax(hists) * 255
+                if np.amax(hists) != 0.0:
+                    hists = hists / np.amax(hists) * 255
                     # print 'hists', hists
                 self.__ahist[i][j][:] = hists
                 # make the 1D vector.
